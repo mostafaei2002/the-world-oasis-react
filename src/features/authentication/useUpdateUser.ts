2be3a6cc1eef47f6ae8bf4721e84799d0bbdc12a
mostafaei2002 <sinamostafaei2002@gmail.com>
2024-02-29 09:36:27 +0330

    update user

---
+++ src/features/authentication/useUpdateUser.ts
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+import { updateCurrentUser } from "../../services/apiAuth";
+
+export function useUpdateUser() {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateUser, isPending: isUpdating } = useMutation({
+    mutationFn: updateCurrentUser,
+    onSuccess: (data) => {
+      console.log(data);
+      toast.success("User account successfully updated.");
+      // queryClient.setQueryData(["user"], data?.user);
+
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { updateUser, isUpdating };
+}
